cfcb9c5c450f2fe834cf8f12096d15c076446e70
naosuke <naosuke@live.jp>
2014-04-15 10:27:31 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,129 @@
+(require 'cl) ;; これがないと何故かうまく動かない
+;; ========================================
+;; package.el
+;; ========================================
+(require 'package)
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; ;; Marmaladeを追加
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; 初期化
+(package-initialize)
+
+;; ========================================
+;; setting
+;; ========================================
+(custom-set-variables
+  '(inhibit-startup-screen t)
+  '(indent-tabs-mode nil)
+  '(menu-bar-mode nil)
+  '(delete-auto-save-files t)
+  '(make-backup-files nil)
+)
+
+;; 対応する括弧を強調
+(show-paren-mode t)
+;; 入力キー表示速度を早くする
+(setq echo-keystrokes 0.1)
+;; ;; C-h をバックスペースに。
+(global-set-key "\C-h" 'backward-delete-char)
+;; 行末の空白を保存時に削除
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; ; タブ幅
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil) ; 空白でインデント
+(defun indent-set ()
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 2 ) ; ブロック内のインデント数
+  (setq tab-width 2 ) ; タブのインデント数
+)
+;; 文字コード
+(set-language-environment "japanese")
+(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+;; ファイル名が重複した時に、バッファ名にディレクトリまで含める
+
+(global-linum-mode t) ; 行番号表示
+
+;;(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-buffer-name-style 'forward)
+;; コマンド履歴の保存 http://qiita.com/akisute3@github/items/4b489c0abbb39a5dcc45
+(setq desktop-globals-to-save '(extended-command-history))
+(setq desktop-files-not-to-save "")
+(desktop-save-mode 1)
+;;--------------------
+;; タブ, 全角スペース, 行末空白表示
+;;--------------------
+;;---
+(defface my-face-b-1 '((t (:background "NavajoWhite4"))) nil) ; 全角スペース
+(defface my-face-b-2 '((t (:background "gray10"))) nil) ; タブ
+(defface my-face-u-1 '((t (:background "SteelBlue" :underline t))) nil) ; 行末空白表示
+(defvar my-face-b-1 'my-face-b-1)
+(defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-u-1 'my-face-u-1)
+(defadvice font-lock-mode (before my-font-lock-mode ())
+ (font-lock-add-keywords major-mode '(("\t" 0 my-face-b-2 append)
+ ("　" 0 my-face-b-1 append)
+ ("[ \t]+$" 0 my-face-u-1 append)
+ )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
+
+;; mark-ring // enable to pop `mark-ring' repeatedly like C-u C-SPC C-SPC ...
+(setq set-mark-command-repeat-pop t)
+;;--------------------
+;; ウィンドウ移動
+;;--------------------
+;; 反対側のｳｨﾝﾄﾞｳにいけるように
+(setq windmove-wrap-around t)
+;; C-M-{p,n,f,b}でｳｨﾝﾄﾞｳ間を移動
+(define-key global-map (kbd "C-M-p") 'windmove-up)
+(define-key global-map (kbd "C-M-n") 'windmove-down)
+(define-key global-map (kbd "C-M-f") 'windmove-right)
+(define-key global-map (kbd "C-M-b") 'windmove-left)
+(global-set-key (kbd "C-x C-o") (lambda () (interactive) (other-window 1))) ; ctrl押しっぱなしでも移動可能
+(global-set-key (kbd "C-x C-n") (lambda () (interactive) (other-window 1))) ; ctrl押しっぱなしでも移動可能
+(global-set-key (kbd "C-x C-p") (lambda () (interactive) (other-window -1))) ; 逆方向の移動
+
+;;--------------------
+;; 色設定
+;;--------------------
+
+(custom-set-faces
+  '(default ((t (:foreground "white" :background "black"))))
+  '(mode-line ((t (:foreground "white" :background "#0044cc" :box nil))))
+  '(mode-line-inactive ((t (:foreground "white" :background "#262626" :box "nil"))))
+)
+
+;; ========================================
+;; helm
+;; ========================================
+(global-set-key (kbd "C-x C-b") 'helm-mini)
+(helm-mode 1)
+;; M-yでキルリングの履歴一覧を表示
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+;; helm modeでもC-hで1文字削除
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+;; tabでディレクトリ移動
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; キーバインドの説明
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
+;; find-file時 C-zでディレクトリ移動
+
+;; ========================================
+;; auto-complete
+;; ========================================
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20140322.321/dict")
+(ac-config-default)
+;; C-n/C-pで候補選択
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
