ca0b36201bf3f9324827f556f389057f01fdec5a
Naoki Hanakawa <naosuke@live.jp>
2014-11-28 15:20:41 +0900

    Gemfile、LockfileをRubyモードで開くように記述

--- init.el
+++ init.el
@@ -182,6 +182,11 @@
 (require 'ruby-tools)
 
 (defun ruby-mode-set-encoding () ())
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
 ;;========================================
 ;; rainbow-delimiters
