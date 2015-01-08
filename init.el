322e26afd5a7c48cec32cbc48c575b6e273dcd17
NaokiHanakawa <naoki.hanakawa@gmail.com>
2015-01-09 03:07:25 +0900

    add google-translate-mode

--- init.el
+++ init.el
@@ -280,3 +280,18 @@
 ;;========================================
 
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+
+;;========================================
+;; Google translte
+;;========================================
+
+(require 'google-translate)
+
+(global-set-key "\C-xt" 'google-translate-at-point)
+(global-set-key "\C-xT" 'google-translate-query-translate)
+
+(custom-set-variables
+ '(google-translate-default-source-language "ja")
+ '(google-translate-default-target-language "en"))
+
+(push '("*Google Translate*") popwin:special-display-config)
