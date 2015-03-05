f8e68467a3726a63b5240084764ed2d2933cbe89
Naoki Hanakawa <naosuke@live.jp>
2015-03-06 08:45:46 +0900

    Go-mode時のインデントをスペースに

--- init.el
+++ init.el
@@ -290,7 +290,12 @@
 (add-to-list 'exec-path (expand-file-name "~/lib/go/bin"))
 (add-hook 'go-mode-hook (lambda()
                           (local-set-key (kbd "M-.") 'godef-jump)))
-
+(add-hook 'go-mode-hook
+          '(lambda()
+             (setq tab-width 2)
+             (setq indent-tabs-mode nil)
+             ))
+; go autocomplete-mode
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
