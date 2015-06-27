5d77b3b2cee476ce3eb58b6f7939778f5e71f672
NaokiHanakawa <naoki.hanakawa@gmail.com>
2015-06-28 04:34:02 +0900

    Org-modeにおいて文字を右端で折り返すよう設定

--- init.el
+++ init.el
@@ -270,6 +270,17 @@
 (volatile-highlights-mode t)
 
 ;;========================================
+;; org mode
+;;========================================
+; 画面端折り返し
+(setq org-startup-truncated nil)
+(defun change-truncation()
+  (interactive)
+  (cond ((eq truncate-lines nil)
+         (setq truncate-lines t))
+        (t
+                  (setq truncate-lines nil))))
+;;========================================
 ;; ace-isearch
 ;;========================================
 
