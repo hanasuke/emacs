3ab81cbfdb42086c25e993ffe307c97f5dfc395a
naosuke <naosuke@live.jp>
2016-11-16 20:55:54 +0900

    旧パッケージ管理用ディレクトリの抹殺

--- elpa/crappy-jsp-mode-20140311.231/crappy-jsp-mode-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; crappy-jsp-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("crappy-jsp-mode-pkg.el" "crappy-jsp-mode.el")
-;;;;;;  (21563 10359 516539 97000))
-
-;;;***
-
-(provide 'crappy-jsp-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; crappy-jsp-mode-autoloads.el ends here
