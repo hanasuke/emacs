150250b3e2b74a2bf3a769153e59eb5d6f435944
naosuke <info@naosuke.me>
2019-05-05 03:26:32 +0900

    :construction: disable popwin

--- inits/03_translate.el
+++ inits/03_translate.el
@@ -1,8 +1,8 @@
 ;; popwin
 
-(require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:popup-window-position 'bottom)
+;; (require 'popwin)
+;; (setq display-buffer-function 'popwin:display-buffer)
+;; (setq popwin:popup-window-position 'bottom)
 
 ;;========================================
 ;; Google translte
