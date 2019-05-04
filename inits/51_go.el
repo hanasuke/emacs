80203df37d4ac06f04971bccb392e0373b16d411
naosuke <info@naosuke.me>
2019-05-05 04:21:35 +0900

    add some keybinds

--- inits/51_go.el
+++ inits/51_go.el
@@ -6,13 +6,20 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-to-list 'exec-path (expand-file-name "/usr/local/go/bin"))
 (add-to-list 'exec-path (expand-file-name "~/lib/go/bin"))
-(add-hook 'go-mode-hook (lambda()
-                          (local-set-key (kbd "M-.") 'godef-jump)))
+
+;; keybind
+(add-hook 'go-mode-hook
+          '(lambda()
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (define-key go-mode-map "\C-c." 'go-test-current-file)
+             (define-key go-mode-map "\C-cf" 'go-test-current-test)
+             (define-key go-mode-map "\C-c\C-n" 'go-run)))
+
 (add-hook 'go-mode-hook
           '(lambda()
              (setq tab-width 2)
-             (setq indent-tabs-mode nil)
-             ))
+             (setq indent-tabs-mode nil)))
+
 ; go autocomplete-mode
 (require 'go-autocomplete)
 (require 'auto-complete-config)
