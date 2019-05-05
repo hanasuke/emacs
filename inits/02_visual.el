eecd0a894904033d50f003949065b1b92e5f92cd
naosuke <info@naosuke.me>
2019-05-05 19:57:11 +0900

    :+1: configure rainbow-delimiters-mode

--- inits/02_visual.el
+++ inits/02_visual.el
@@ -106,7 +106,14 @@
 ;;========================================
 
 (require 'rainbow-delimiters)
-(rainbow-delimiters-mode t)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(require 'cl-lib)
+(require 'color)
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 30)))
 
 ;;========================================
 ;; rainbow-mode
